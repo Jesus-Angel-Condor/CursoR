9e66a7d1e2af2ac1649b337816658e7b6f340245
Jesus-Angel-Condor <pablo_cf23@hotmail.com>
2017-09-14 22:38:45 -0500

    Archivo Enviado

---
+++ ejemplo3.py
@@ -0,0 +1,12 @@
+numeros = {1,5,2,12,,14,7,18}
+
+doble = []
+for numero in numeros:
+    doble.append(2*numero)
+
+numeros_pares = []
+for numero in numeros:
+    if numero % 2 == 0:
+        numeros_pares.append(numero)
+
+animalitos = ['aardvark','cat','dog','opposum']

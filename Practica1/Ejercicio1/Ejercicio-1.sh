add8ff6fed4b799d5f2e8524f0c6b49fad49aedd
Jesus-Angel-Condor <pablo_cf23@hotmail.com>
2017-09-29 13:56:57 -0500

    Archivo enviado

---
+++ Practica1/Ejercicio1/Ejercicio-1.sh
@@ -0,0 +1,26 @@
+#Ejercicio 1
+#Nombre: Jesus Condor Torres 20141378D
+#Respuestas
+#Item a
+#Esta pregunta nos dicce que creemos un archivo llamado "examen" y contenga
+#Erika Rios : 20
+#Jessica Martinez :32
+#Claudio Lara : 25
+examen <- paste(c("Erika Rios","Jessica Martinez Figueroa","claudio Lara Avila"),c(20,32,25),sep=":")
+examen
+#Ahora que me dijo sobre lineas de comando entonces
+#el archivo se puede iniciar con un funcion "paste",donde colocamos dentro de esa funcion
+#vector de tres nombres y otro de 3 numeros con la funcion sep para poder poner ":" 
+#Item b
+#En esta pregunta nos pide ordenar el arhivo examen y gurdarlo en temp
+help("order")
+order(c(20,32,25))
+#Para el segundo caso podemos ordenar el archivo anterior con la funcion "order"
+#respecto de las edades y lo almacenamos en temp de esta forma
+#temp <- order(examen)
+#Item c
+#
+#Item d
+#En esta pregunta nos pide quecomandos  podemos usar para crear nuevos documentos
+#con la extension .sh y .txt
+#un comando 
